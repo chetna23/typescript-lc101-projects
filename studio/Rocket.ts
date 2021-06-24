88a8b8536c96df9aa84f0a75f165b58b9c9bf055
Chetna Aggarwal <caggarw@monsanto.com>
2021-06-23 19:06:22 -0500

    added code

---
+++ studio/Rocket.ts
@@ -0,0 +1,44 @@
+import { Astronaut } from "./Astronaut";
+import { Cargo } from "./Cargo";
+import { Payload } from "./Payload";
+
+export class Rocket {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
+
+    constructor(name: string, totalCapacityKg: number) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    sumMass( items: Payload[] ): number {
+        let totalMass:number = 0;
+        for (let index = 0; index < items.length; index++) {
+            totalMass += items[index].massKg;
+        }
+        return totalMass;
+    }
+
+    currentMassKg(): number {
+        return this.sumMass(this.cargoItems) + this.sumMass(this.astronauts);
+    }
+
+    canAdd(item: Payload):boolean {
+        return this.currentMassKg() + item.massKg <= this.totalCapacityKg;
+    }
+
+    addCargo(cargo: Cargo): boolean {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        } else return false;
+    }
+
+    addAstronaut(astronaut: Astronaut): boolean {
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        } else return false;
+    }
+}
